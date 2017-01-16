dfdce40fa8fa4b7dc63ffcf5658bd9ee09fe29b1
Peter Foley <pefoley2@pefoley.com>
2017-01-15 20:25:28 -0500

    logging

--- cross.py
+++ cross.py
@@ -8,20 +8,26 @@ import subprocess
 import sys
 import warnings
 
-from typing import Any, Sequence, Tuple, Union
+from typing import Any, List, Sequence, Tuple, Union
 
 warnings.simplefilter('default')
 
 _GCC_URL = 'git://gcc.gnu.org/git/gcc.git'
 _BINUTILS_URL = 'git://sourceware.org/git/binutils-gdb.git'
+
 _DIR = os.path.dirname(os.path.abspath(__file__))
+
+_LOG_DIR = os.path.join(_DIR, 'logs')
+_BINUTILS_LOG = os.path.join(_LOG_DIR, 'binutils-{}.log')
+_GCC_LOG = os.path.join(_LOG_DIR, 'gcc-{}.log')
+
 _SRC_DIR = os.path.join(_DIR, 'src')
-_GCC_SRC = os.path.join(_SRC_DIR, 'gcc')
 _BINUTILS_SRC = os.path.join(_SRC_DIR, 'binutils')
+_GCC_SRC = os.path.join(_SRC_DIR, 'gcc')
 
 _WORK_DIR = os.path.join(_DIR, 'work')
-_GCC_WORK = os.path.join(_WORK_DIR, '{}-gcc')
-_BINUTILS_WORK = os.path.join(_WORK_DIR, '{}-binutils')
+_BINUTILS_WORK = os.path.join(_WORK_DIR, 'binutils-{}')
+_GCC_WORK = os.path.join(_WORK_DIR, 'gcc-{}')
 
 
 class CrossException(Exception):
@@ -75,30 +81,54 @@ class Builder(object):
         self.is_cross = self.host != self.target
 
     def fetch(self) -> None:
-        if not os.path.exists(_GCC_SRC):
-            subprocess.run(['git', 'clone', _GCC_URL, _GCC_SRC], check=True)
+        for src, url in [(_BINUTILS_SRC, _BINUTILS_URL), (_GCC_SRC, _GCC_URL)]:
+            if not os.path.exists(src):
+                subprocess.run(['git', 'clone', url, src], check=True)
+            subprocess.run(['git', 'pull'], check=True, cwd=src)
 
-    def format_args(self, work: str, target: Target) -> Tuple[str, List[str]]:
-        work_dir = args = None
+    def format_args(self, work: str, target: Target) -> Tuple[str, str, List[str]]:
+        log = work.replace(_WORK_DIR, _LOG_DIR)
+        log_dir = work_dir = args = None
         if target == Target.HOST:
             work_dir = work.format(self.host)
+            log_dir = log.format(self.host)
             args = get_args(self.build, self.build, self.host)
         elif target == Target.TARGET:
             work_dir = work.format(self.target)
+            log_dir = log.format(self.target)
             args = get_args(self.build, self.build, self.target)
         elif target == Target.CANADIAN:
-            work_dir = work.format("-".join([self.host, self.target]))
+            name = "_".join([self.host, self.target])
+            work_dir = work.format(name)
+            log_dir = log.format(name)
             args = get_args(self.build, self.host, self.target)
         else:
             raise CrossException("You shouldn't be building GCC for build.")
-        return work_dir, args
+        return log_dir, work_dir, args
+
+    def configure_pkg(self, src: str, log: str, work: str, args: List[str]) -> None:
+        with open(log, 'w') as f:
+            proc = subprocess.Popen(
+                [os.path.join(src, 'configure')] + args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+                cwd=work)
+            for line in proc.stdout:
+                sys.stdout.write(line)
+                f.write(line)
+            if proc.wait():
+                raise CrossException('Configuration of {} failed.'.format(src))
 
     def build_pkg(self, src: str, work: str, target: Target) -> None:
-        work_dir, args = self.format_args(work, target)
+        log_dir, work_dir, args = self.format_args(work, target)
+        if not os.path.exists(_LOG_DIR):
+            os.makedirs(_LOG_DIR)
         if not os.path.exists(work_dir):
             os.makedirs(work_dir)
+
         if not os.path.exists(os.path.join(work_dir, 'Makefile')):
-            subprocess.run([os.path.join(src, 'configure')] + args, check=True, cwd=work_dir)
+            self.configure_pkg(src, log_dir, work_dir, args)
         subprocess.run(self.make_cmd, check=True, cwd=work_dir)
 
     def build_gcc(self, target: Target) -> None:
