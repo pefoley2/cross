6c1ae7e11e80b16a3ecc38d89bf21f1b12317dc3
Peter Foley <pefoley2@pefoley.com>
2017-01-15 21:34:48 -0500

    refactoring

--- cross.py
+++ cross.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import collections
 import enum
 import os
 import subprocess
@@ -12,22 +13,24 @@ from typing import Any, List, Sequence, Tuple, Union
 
 warnings.simplefilter('default')
 
-_GCC_URL = 'git://gcc.gnu.org/git/gcc.git'
-_BINUTILS_URL = 'git://sourceware.org/git/binutils-gdb.git'
+_PKGS = collections.defaultdict(dict) # type: Dict[str, Dict[str, str]]
+
+_PKGS['binutils']['url'] = 'git://sourceware.org/git/binutils-gdb.git'
+_PKGS['gcc']['url'] = 'git://gcc.gnu.org/git/gcc.git'
 
 _DIR = os.path.dirname(os.path.abspath(__file__))
 
 _LOG_DIR = os.path.join(_DIR, 'logs')
-_BINUTILS_LOG = os.path.join(_LOG_DIR, 'binutils-{}.log')
-_GCC_LOG = os.path.join(_LOG_DIR, 'gcc-{}.log')
+_PKGS['binutils']['log'] = os.path.join(_LOG_DIR, 'binutils-{}-{}.log')
+_PKGS['gcc']['log'] = os.path.join(_LOG_DIR, 'gcc-{}-{}.log')
 
 _SRC_DIR = os.path.join(_DIR, 'src')
-_BINUTILS_SRC = os.path.join(_SRC_DIR, 'binutils')
-_GCC_SRC = os.path.join(_SRC_DIR, 'gcc')
+_PKGS['binutils']['src'] = os.path.join(_SRC_DIR, 'binutils')
+_PKGS['gcc']['src'] = os.path.join(_SRC_DIR, 'gcc')
 
 _WORK_DIR = os.path.join(_DIR, 'work')
-_BINUTILS_WORK = os.path.join(_WORK_DIR, 'binutils-{}')
-_GCC_WORK = os.path.join(_WORK_DIR, 'gcc-{}')
+_PKGS['binutils']['work'] = os.path.join(_WORK_DIR, 'binutils-{}')
+_PKGS['gcc']['work'] = os.path.join(_WORK_DIR, 'gcc-{}')
 
 
 class CrossException(Exception):
@@ -44,6 +47,40 @@ def get_build() -> str:
 
 def get_args(build: str, host: str, target: str) -> List[str]:
     return ['--build={}'.format(build), '--host={}'.format(host), '--target={}'.format(target)]
+
+
+def fetch() -> None:
+    for pkg in _PKGS.values():
+        if not os.path.exists(pkg['src']):
+            subprocess.run(['git', 'clone', '--branch', 'master', pkg['url'], pkg['src']],
+                           check=True)
+        subprocess.run(['git', 'pull'], check=True, cwd=pkg['src'])
+
+
+def get_log_path(pkg: str, triple: str, action: str) -> str:
+    return _PKGS[pkg]['log'].format(triple, action)
+
+
+def run_command(args: List[str], log_path: str, work_dir: str) -> None:
+    try:
+        with open(log_path, 'w') as log_file:
+            proc = subprocess.Popen(
+                args,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+                cwd=work_dir)
+            for line in proc.stdout:
+                sys.stdout.write(line)
+                log_file.write(line)
+            proc.stdout.close()
+        if proc.wait():
+            raise CrossException('Command {} failed.'.format(' '.join(args)))
+    except:
+        raise
+    finally:
+        if hasattr(proc, 'stdout'):
+            proc.stdout.close()
 
 
 class Canonicalize(argparse.Action):
@@ -80,75 +117,46 @@ class Builder(object):
         self.is_canadian = self.build != self.host
         self.is_cross = self.host != self.target
 
-    @staticmethod
-    def fetch() -> None:
-        for src, url in [(_BINUTILS_SRC, _BINUTILS_URL), (_GCC_SRC, _GCC_URL)]:
-            if not os.path.exists(src):
-                subprocess.run(['git', 'clone', '--branch', 'master', url, src], check=True)
-            subprocess.run(['git', 'pull'], check=True, cwd=src)
-
-    def format_args(self, log: str, work: str, target: Target) -> Tuple[str, str, List[str]]:
-        log_dir = work_dir = args = None
+    def format_args(self, pkg: str, target: Target) -> Tuple[str, str, List[str]]:
+        work = _PKGS[pkg]['work']
+        name = work_dir = args = None
         if target == Target.HOST:
+            name = self.host
             work_dir = work.format(self.host)
-            log_dir = log.format(self.host)
             args = get_args(self.build, self.build, self.host)
         elif target == Target.TARGET:
+            name = self.target
             work_dir = work.format(self.target)
-            log_dir = log.format(self.target)
             args = get_args(self.build, self.build, self.target)
         elif target == Target.CANADIAN:
             name = "_".join([self.host, self.target])
             work_dir = work.format(name)
-            log_dir = log.format(name)
             args = get_args(self.build, self.host, self.target)
         else:
-            raise CrossException("You shouldn't be building GCC for build.")
-        return log_dir, work_dir, args
+            raise CrossException("You shouldn't be building anything for build.")
+        return name, work_dir, args
 
-    @staticmethod
-    def configure_pkg(src: str, log: str, work: str, args: List[str]) -> None:
-        log_file = open(log, 'w')
-        proc = subprocess.Popen(
-            [os.path.join(src, 'configure')] + args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            universal_newlines=True,
-            cwd=work)
-        for line in proc.stdout:
-            sys.stdout.write(line)
-            log_file.write(line)
-        proc.stdout.close()
-        log_file.close()
-        if proc.wait():
-            raise CrossException('Configuration of {} failed.'.format(src))
-
-    def build_pkg(self, src: str, log: str, work: str, target: Target) -> None:
-        log_dir, work_dir, args = self.format_args(log, work, target)
+    def build_pkg(self, pkg: str, target: Target) -> None:
+        triple, work_dir, config_args = self.format_args(pkg, target)
         if not os.path.exists(_LOG_DIR):
             os.makedirs(_LOG_DIR)
         if not os.path.exists(work_dir):
             os.makedirs(work_dir)
 
         if not os.path.exists(os.path.join(work_dir, 'Makefile')):
-            self.configure_pkg(src, log_dir, work_dir, args)
-        subprocess.run(self.make_cmd, check=True, cwd=work_dir)
-
-    def build_gcc(self, target: Target) -> None:
-        self.build_pkg(_GCC_SRC, _GCC_LOG, _GCC_WORK, target)
-
-    def build_binutils(self, target: Target) -> None:
-        self.build_pkg(_BINUTILS_SRC, _BINUTILS_LOG, _BINUTILS_WORK, target)
+            args = [os.path.join(_PKGS[pkg]['src'], 'configure')] + config_args
+            run_command(args, get_log_path(pkg, triple, 'config'), work_dir)
+        run_command(self.make_cmd, get_log_path(pkg, triple, 'build'), work_dir)
 
     def compile(self) -> None:
         if self.is_cross:
-            self.build_binutils(Target.TARGET)
-            self.build_gcc(Target.TARGET)
+            self.build_pkg('binutils', Target.TARGET)
+            self.build_pkg('gcc', Target.TARGET)
         if self.is_canadian:
-            self.build_binutils(Target.HOST)
-            self.build_gcc(Target.HOST)
-            self.build_binutils(Target.CANADIAN)
-            self.build_gcc(Target.CANADIAN)
+            self.build_pkg('binutils', Target.HOST)
+            self.build_pkg('gcc', Target.HOST)
+            self.build_pkg('binutils', Target.CANADIAN)
+            self.build_pkg('gcc', Target.CANADIAN)
 
 
 def main() -> None:
@@ -161,9 +169,8 @@ def main() -> None:
     args = parser.parse_args()
 
     print('build: {}, host: {}, target: {}'.format(args.build, args.host, args.target))
-    builder = Builder(args)
-    builder.fetch()
-    builder.compile()
+    fetch()
+    Builder(args).compile()
 
 
 if __name__ == '__main__':
